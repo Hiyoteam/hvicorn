70a30948bf2b32b67cbb569fd89cef7494142969
0x24a <tanhanze@qq.com>
2024-06-05 00:15:13 +0800

    Updates demo

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from hvicorn import Bot, CommandContext, threaded
 from hvicorn.models.server import ChatPackage
 from logging import basicConfig, DEBUG
-import time
+import time, random
 
 basicConfig(level=DEBUG)
 
@@ -11,9 +11,7 @@ my_bot = Bot("test_hvicorn", "lounge")
 @my_bot.startup
 @threaded
 def greetings(bot: Bot):
-    message = bot.send_message("Hello world!", editable=True)
-    time.sleep(5)
-    message.append("And everyone!")
+    bot.send_message("Hello world! I am hvicorn demo bot.\nCommands:\n\t`.hv editmsg` - demos updatemessage.\n\t`.hv invite` - demos inviting.\n\t`.hv emote` - demos emote.\n\tSpecial command: try sending awa")
 
 
 @my_bot.on(ChatPackage)
@@ -28,9 +26,19 @@ def on_chat(bot: Bot, msg: ChatPackage):
         bot.emote(f"hugs {msg.nick}")
 
 
-@my_bot.command(".examplecommand")
-def examplecommand(ctx: CommandContext):
-    ctx.respond("Hello world")
+@my_bot.command(".hv editmsg")
+@threaded
+def editmsg(ctx: CommandContext):
+    msg=ctx.bot.send_message("Do you like playing ", editable=True)
+    time.sleep(5)
+    msg.append(random.choice(["Genshin impact","Honkai impact","Minecraft","Project sekai"])+"?")
 
+@my_bot.command(".hv invite")
+def invite(ctx: CommandContext):
+    ctx.bot.invite(ctx.triggered_by.nick, "somechannel")
+
+@my_bot.command(".hv emote")
+def emote(ctx: CommandContext):
+    ctx.bot.emote("awa")
 
 my_bot.run()

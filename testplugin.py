40edcb4942d6fe804fdfdaf6af7efd2c6436f12f
0x24a <tanhanze@qq.com>
2024-07-12 11:16:29 +0800

    Update command demo

--- testplugin.py
+++ testplugin.py
@@ -2,6 +2,6 @@ import hvicorn
 
 
 def plugin_init(bot: hvicorn.Bot, command_name: str):
-    @bot.command(command_name)
     def hello(ctx: hvicorn.CommandContext):
         ctx.respond("Hello from a hvicorn plugin")
+    bot.register_command(command_name, hello)

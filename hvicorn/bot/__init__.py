7f7f26606e0e5452c0466988b3f20dab4c1685d7
0x24a <tanhanze@qq.com>
2024-08-02 23:11:05 +0800

    Change filenames

--- hvicorn/bot/__init__.py
+++ hvicorn/bot/__init__.py
@@ -1,2 +1,2 @@
-from hvicorn.bot.bot import Bot, CommandContext, threaded
-from hvicorn.bot.asyncbot import AsyncBot, AsyncCommandContext
+from hvicorn.bot.sync_client import Bot, CommandContext, threaded
+from hvicorn.bot.async_client import AsyncBot, AsyncCommandContext

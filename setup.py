ef4905ce176df6e7aa423059e3ef8b3a641efb52
0x24a <tanhanze@qq.com>
2024-08-03 12:32:08 +0800

    Bump version to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="hvicorn",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple hack.chat bot framework with type hinting",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
